f60aac74a09c58e0b121b8c63a272ac33d57ba14
sophierahn <sophierahn@hotmail.com>
2022-10-26 11:02:30 -0400

    Creating the jupyter notebook file

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,5 @@
+#Testing out committing to github from VS code
+#Doing this on windows instead of a linux machine is the worst
 print("is this thing on?")
 print("Github, do you read?")
 print("Making some more changes")
